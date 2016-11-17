d2a1fa4d929ec12276e3da7b933c8cc2e2ac875d
AlexRZH <ryzhii.o.rzh@gmail.com>
2016-11-17 11:22:36 +0200

    Android Developers training course

---
+++ MyFirstApp/app/src/main/java/com/example/user/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,14 @@
+package com.example.user.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by user on 13.11.2016.
+ */
+public class DisplayMessageActivity {
+
+
+
+}
